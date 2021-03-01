0d0326f702ba5cde0a81cc1abe253d1e8b03cf51
majakesin <kesin.maja@gmail.com>
2021-03-01 21:56:13 +0100

    gitignore fixed

--- djangoEnd/movie/views.py
+++ djangoEnd/movie/views.py
@@ -20,9 +20,9 @@ from .filters import MovieFilter
 from .models import Movie, Genre, LikeDislikeOption, Comments, WatchedMovies
 from .serializers import MovieSerializer, GenreSerializer, CommentsSerializer, MovieSerializerPopular, \
     MovieRelatedSerializer, MovieElasticSearchSerializer
-#from djangoEnd.jwtAuthentication.backends import JWTAuthentication
+from djangoEnd.jwtAuthentication.backends import JWTAuthentication
 
-#from djangoEnd import settings
+from djangoEnd import settings
 
 
 class PaginationMovie(PageNumberPagination):
@@ -42,10 +42,10 @@ class GenreView(mixins.ListModelMixin, GenericViewSet):
     serializer_class = GenreSerializer
 
 
-#def send_email_fun(subject, content, email_to):
- #   email = EmailMessage(subject, content, settings.EMAIL_HOST_USER, email_to)
-  #  email.fail_silenty = False
-   # email.send()
+def send_email_fun(subject, content, email_to):
+    email = EmailMessage(subject, content, settings.EMAIL_HOST_USER, email_to)
+    email.fail_silenty = False
+    email.send()
 
 
 class MovieView(mixins.ListModelMixin,
@@ -66,9 +66,7 @@ class MovieView(mixins.ListModelMixin,
         cover_image = request.FILES['cover_image']
         admin = User.objects.get(is_superuser=1)
         content = f"New movie has been created. Movie title is: {title} Movie description is: {description}"
-    #    send_email_fun("Created new movie", content, admin)
-        # mora ovako, jer nece vezu vise na vise da sacuva bez id filma,pa se on mora prvi sacuvati,pa
-        # tek onda dodati zanrovi
+        send_email_fun("Created new movie", content, admin)
         movieSave = Movie(title=title, description=description, cover_image=cover_image)
         movieSave.save()
         movieSave.genres.set(genres)
@@ -116,13 +114,13 @@ class LikeDislikeView(GenericAPIView):
 
     def post(self, request):
         data = request.data;
-     #   user, token = JWTAuthentication.authenticate(self, request)
+        user, token = JWTAuthentication.authenticate(self, request)
 
         if not LikeDislikeOption.objects.filter(user_id=user, movie_id=data['movie_id']).exists():
             ld = LikeDislikeOption()
             ld.movie_id = data['movie_id']
             ld.type = data['type']
-      #      ld.user_id = user
+            ld.user_id = user
             ld.save()
             return Response(data, status=status.HTTP_200_OK)
 
@@ -149,16 +147,15 @@ class WatchedMovieView(mixins.ListModelMixin,
     def create(self, request, *args, **kwargs):
         movie_id = request.data['movie_id']
         movie = Movie.objects.get(id=movie_id)
-       # username, token = JWTAuthentication.authenticate(self, request)
-        #user = User.objects.get(username=username)
+        username, token = JWTAuthentication.authenticate(self, request)
+        user = User.objects.get(username=username)
         watchedMovie = WatchedMovies()
         watchedMovie.movie = movie
-        #watchedMovie.user = user
+        watchedMovie.user = user
         watchedMovie.save()
         return Response(status=status.HTTP_200_OK)
 
 
-# primer za elasticsearch
 class MovieElasticSearchView(BaseDocumentViewSet):
     document = MovieDocument
     serializer_class = MovieElasticSearchSerializer
